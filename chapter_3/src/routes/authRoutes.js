9d621f179c9580ceffbcc6aa9c5182df7d945fac
Alex-935 <alex.potter3@hotmail.co.uk>
2026-05-24 12:53:21 +0100

    NodeJS & SQLite

---
+++ chapter_3/src/routes/authRoutes.js
@@ -0,0 +1,15 @@
+
+import express from 'express';
+import bcrypt from 'bcryptjs';// encrypter for passwords
+import jwt from 'jsonwebtoken';// generates json token - secure alphanumeric passowrd we can assosicate with a user for when they login again
+import db from '../db';
+
+
+// configuring endpoints outside the original file
+const router = express.router();
+
+
+router.post('./')
+
+
+export default router;
--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -6,10 +6,12 @@ import { fileURLToPath } from 'url';
 // to use this version of import, we need to do to package.json and cange the line
 //      > "type": "module"
 // CommonJS by default but this overwrites it to ES Modules
+import authRoutes from './authRoutes.js';
 
 const app = express();
 // checks if there's a port environment variable, not use 5000
 const PORT = process.env.PORT || 5000;
+
 
 // get the file path from the URL of the current directory
 const __filename = fileURLToPath(import.meta.url);//gets file name
@@ -23,6 +25,7 @@ app.use(express.json());
 // Serves the HTML file from the /public directory
 // tells express to serve all files from the public folder as static assets/files
 // Any requests for the css will be resolved to the public directory
+// without this, it defaults the route to the /src folder
 app.use(express.static(path.join(__dirname, '../public')));
 
 
@@ -33,6 +36,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
+// routes
+// for any path that has /auth, use the authRoutes file
+app.use('./auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server has started on port: ${PORT}`);
